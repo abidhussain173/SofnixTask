3cdc9ca9df98d61bfba6fbcce9af2ad40b7b1a18
abidhussain173 <abidghouri173@gmail.com>
2023-10-04 16:50:27 +0500

    first commint

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ProductListingScreen from './src/products/ProductListingScreen'
+
+const App = () => {
+  return (
+    <View>
+      <ProductListingScreen/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/products/CountDownTimer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CountDownTimer = () => {
+  return (
+    <View>
+      <Text>CountDownTimer</Text>
+    </View>
+  )
+}
+
+export default CountDownTimer
+
+const styles = StyleSheet.create({})
