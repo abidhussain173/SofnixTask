a394d9cbc24b471bb953d89197e37455c697c8d5
abidhussain173 <62146615+abidhussain173@users.noreply.github.com>
2023-10-05 18:11:32 +0500

    UI Fixes

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,151 +1,176 @@
-import React, { memo, useEffect, useState, useRef } from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
-import { ProductListProps } from './types';
+import React, {memo, useEffect, useState, useRef} from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+import {ProductListProps} from './types';
 
-const ProductList: React.FC<ProductListProps> = ({ data }) => {
-    const [totalSeconds, setTotalSeconds] = useState<number>(data.timeInSeconds);
-    const [isActive, setIsActive] = useState<boolean>(true);
-    const [counterCompleted, setCounterCompleted] = useState<boolean>(false);
-    const [productPrice, setProductPrice] = useState<number | null>(null);
-    const [productName, setProductName] = useState<string | null>(null);
-    const timer = useRef<any | null>(null);
+const ProductList: React.FC<ProductListProps> = ({data}) => {
+  const [totalSeconds, setTotalSeconds] = useState<number>(data.timeInSeconds);
+  const [isActive, setIsActive] = useState<boolean>(true);
+  const [counterCompleted, setCounterCompleted] = useState<boolean>(false);
+  const [productPrice, setProductPrice] = useState<number | null>(null);
+  const [productName, setProductName] = useState<string | null>(null);
+  const timer = useRef<any | null>(null);
 
-    useEffect(() => {
-        if (isActive && totalSeconds > 0) {
-            timer.current = setInterval(() => {
-                setTotalSeconds((prev) => prev - 1);
-            }, 1000);
-        } else {
-            clearInterval(timer.current!);
-            if (totalSeconds === 0) {
-                setCounterCompleted(true);
-            }
-        }
+  useEffect(() => {
+    if (isActive && totalSeconds > 0) {
+      timer.current = setInterval(() => {
+        setTotalSeconds(prev => prev - 1);
+      }, 1000);
+    } else {
+      clearInterval(timer.current!);
+      if (totalSeconds === 0) {
+        setCounterCompleted(true);
+      }
+    }
 
-        return () => {
-            if (timer.current) {
-                clearInterval(timer.current);
-            }
-        };
-    }, [isActive, totalSeconds]);
-
-    useEffect(() => {
-        if (counterCompleted) {
-            fetch(`http://3.223.25.80:8080/TestAPI/api/Product/GetProductDetailById?ProductId=${data.productId}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    setProductPrice(data.price);
-                    console.log(data);
-                    setProductName(data.productName);
-                })
-                .catch((error) => {
-                    console.error('Error fetching product details:', error);
-                });
-        }
-    }, [counterCompleted, data.productId]);
-
-    const formatTime = (totalSeconds: number): string => {
-        let hours: string | number = Math.floor(totalSeconds / 3600);
-        let minutes: string | number = Math.floor((totalSeconds % 3600) / 60);
-        let remainingSeconds: string | number = totalSeconds % 60;
-
-        hours = hours < 10 ? '0' + hours : hours.toString();
-        minutes = minutes < 10 ? '0' + minutes : minutes.toString();
-        remainingSeconds = remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds.toString();
-
-        return hours + ':' + minutes + ':' + remainingSeconds;
+    return () => {
+      if (timer.current) {
+        clearInterval(timer.current);
+      }
     };
+  }, [isActive, totalSeconds]);
 
+  useEffect(() => {
+    if (counterCompleted) {
+      fetch(
+        `http://3.223.25.80:8080/TestAPI/api/Product/GetProductDetailById?ProductId=${data.productId}`,
+      )
+        .then(response => response.json())
+        .then(data => {
+          setProductPrice(data.price);
+          console.log(data);
+          setProductName(data.productName);
+        })
+        .catch(error => {
+          console.error('Error fetching product details:', error);
+        });
+    }
+  }, [counterCompleted, data.productId]);
 
-    const handlePauseResume = (): void => {
-        setIsActive(!isActive);
-    };
+  const formatTime = (totalSeconds: number): string => {
+    let hours: string | number = Math.floor(totalSeconds / 3600);
+    let minutes: string | number = Math.floor((totalSeconds % 3600) / 60);
+    let remainingSeconds: string | number = totalSeconds % 60;
 
-    const handleReset = (): void => {
-        setIsActive(false);
-        setTotalSeconds(data.timeInSeconds);
-        setCounterCompleted(false);
-        setProductPrice(null); 
-        setProductName(null);
-    };
+    hours = hours < 10 ? '0' + hours : hours.toString();
+    minutes = minutes < 10 ? '0' + minutes : minutes.toString();
+    remainingSeconds =
+      remainingSeconds < 10
+        ? '0' + remainingSeconds
+        : remainingSeconds.toString();
 
-    return (
-        <View style={styles.card}>
-            <View style={styles.cardView}>
-                <View>
-                    <Text style={styles.cardText}>ProductID: {data.productId}</Text>
-                </View>
-                <View>
-                    {counterCompleted ? (
-                        <View>
+    return hours + ':' + minutes + ':' + remainingSeconds;
+  };
 
-                            <Text style={styles.priceText}>Price: {productPrice !== null ? `${productPrice.toFixed(2)}` : 'Loading...'}</Text>
-                            <Text style={styles.priceText}>Price: {productName !== null ? `${productName}` : 'Loading...'}</Text>
-                        </View>
-                    ) : (
-                        <>
-                            <Text style={styles.timer}>{formatTime(totalSeconds)}</Text>
-                            <View style={styles.btnsView}>
-                                <Pressable onPress={handlePauseResume} style={styles.btn}>
-                                    <Text style={styles.btnText}>{isActive ? 'PAUSE' : 'RESUME'}</Text>
-                                </Pressable>
-                                <Pressable onPress={handleReset} style={styles.btn}>
-                                    <Text style={styles.btnText}>RESET</Text>
-                                </Pressable>
-                            </View>
-                        </>
-                    )}
-                </View>
-            </View>
+  const handlePauseResume = (): void => {
+    setIsActive(!isActive);
+  };
+
+  const handleReset = (): void => {
+    setIsActive(false);
+    setTotalSeconds(data.timeInSeconds);
+    setCounterCompleted(false);
+    setProductPrice(null);
+    setProductName(null);
+  };
+
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardView}>
+        <View>
+          <Text style={styles.cardText}>ProductID: {data.productId}</Text>
         </View>
-    );
+        <View>
+          {counterCompleted ? (
+            <View style={styles.priceView}>
+              <Text style={styles.priceText}>
+                Price: {productName !== null ? `${productName}` : 'Loading...'}
+              </Text>
+              <Text style={styles.priceText}>
+                Price:{' '}
+                {productPrice !== null
+                  ? `${productPrice.toFixed(2)}`
+                  : 'Loading...'}
+              </Text>
+            </View>
+          ) : (
+            <>
+              <View style={styles.timerView}>
+                <Text style={styles.timer}>{formatTime(totalSeconds)}</Text>
+                <View style={styles.btnsView}>
+                  <Pressable onPress={handlePauseResume} style={styles.btn}>
+                    <Text style={styles.btnText}>
+                      {isActive ? 'PAUSE' : 'RESUME'}
+                    </Text>
+                  </Pressable>
+                  <Pressable onPress={handleReset} style={styles.btn}>
+                    <Text style={styles.btnText}>RESET</Text>
+                  </Pressable>
+                </View>
+              </View>
+            </>
+          )}
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default memo(ProductList);
 
 const styles = StyleSheet.create({
-    card: {
-        flex: 1,
-        backgroundColor: '#fff',
-        padding: 10,
-        paddingVertical: 20,
-        borderBottomWidth: 5,
-        borderBottomColor: 'black',
-    },
-    cardView: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    btnsView: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    btn: {
-        paddingHorizontal: 5,
-        borderWidth: 1,
-        borderColor: 'black',
-        marginHorizontal: 5,
-    },
-    cardText: {
-        color: '#000000',
-        fontSize: 14,
-    },
-    timer: {
-        alignSelf: 'center',
-        marginBottom: 10,
-        fontWeight: 'bold',
-        color: 'black',
-    },
-    btnText: {
-        color: 'black',
-    },
-    priceText: {
-        alignSelf:'flex-start',
-        marginBottom: 10,
-        fontWeight: 'bold',
-        color: 'green', 
-    },
+  card: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 10,
+    paddingVertical: 10,
+    borderBottomWidth: 5,
+    borderBottomColor: 'black',
+  },
+  cardView: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignContent: 'center',
+  },
+  priceView: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  btnsView: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    paddingHorizontal: 5,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginHorizontal: 5,
+  },
+  cardText: {
+    color: '#000000',
+    fontSize: 14,
+  },
+  timerView: {
+    flex: 1,
+    alignSelf: 'flex-end',
+  },
+  timer: {
+    alignSelf: 'center',
+    marginBottom: 15,
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 20,
+    marginTop: -15,
+  },
+  btnText: {
+    color: 'black',
+  },
+  priceText: {
+    alignSelf: 'flex-start',
+    marginBottom: 10,
+    color: 'black',
+  },
 });
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,78 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import axios from 'axios';
 
 import ProductList from '../../components/ProductList';
-import { Product } from './types';
+import {Product} from './types';
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState<Product[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        axios.get("http://3.223.25.80:8080/rentole-api/api/Product/GetProductIds")
-            .then((response) => {
-                setProducts(response.data);
-                setLoading(false);
-            })
-            .catch((error) => {
-                setError('Error fetching data. Please try again later.');
-                setLoading(false);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get('http://3.223.25.80:8080/rentole-api/api/Product/GetProductIds')
+      .then(response => {
+        setProducts(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError('Error fetching data. Please try again later.');
+        setLoading(false);
+      });
+  }, []);
 
-    return (
-        <SafeAreaView style={styles.container}>
-            {loading ? (
-                <View style={styles.loadingContainer}>
-                    <Text>Loading...</Text>
-                </View>
-            ) : error ? (
-                <View style={styles.errorContainer}>
-                    <Text style={styles.errorText}>{error}</Text>
-                </View>
-            ) : products.length === 0 ? (
-                <View style={styles.emptyContainer}>
-                    <Text style={styles.emptyText}>No products found.</Text>
-                </View>
-            ) : (
-                <FlatList
-                    data={products}
-                    keyExtractor={(item) => item.productId.toString()}
-                    renderItem={({ item }) => <ProductList data={item} />}
-                />
-            )}
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      {loading ? (
+        <View style={styles.loadingContainer}>
+          <Text>Loading...</Text>
+        </View>
+      ) : error ? (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{error}</Text>
+        </View>
+      ) : products.length === 0 ? (
+        <View style={styles.emptyContainer}>
+          <Text style={styles.emptyText}>No products found.</Text>
+        </View>
+      ) : (
+        <FlatList
+          data={products}
+          keyExtractor={item => item.productId.toString()}
+          renderItem={({item}) => <ProductList data={item} />}
+        />
+      )}
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    loadingContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    errorContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    errorText: {
-        color: 'red',
-    },
-    emptyContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    emptyText: {
-        color: 'gray',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorText: {
+    color: 'red',
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyText: {
+    color: 'gray',
+  },
 });
 
 export default HomeScreen;
